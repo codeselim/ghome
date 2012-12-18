8c827b7a97e31b7870a8787d771fc99365e23cd7
Troll <trolldu26@gmail.com>
2012-12-18 11:53:29 +0100

    Able to load a web page and change it from js. now beginning an attempt to use server sent events in JS

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 var http = require('http');
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
+
 var homeTemplate = null;
+
+URLS = []
+URLS["page2"] = "/page2.html"
+URLS["SSE"] = "/sse.html"
+
+function constructSSE(argument) {
+	// @TODO
+}
+
+function sendSSE (argument) {
+	// @TODO
+}
 
 http.createServer(function (req, res) {
 	//Note : req is an instance of http.ServerRequest and res is an instance of http.ServerResponse
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	try {
+		urlFile = req.url.replace(/([^\?]+)\?(.*)/, "$1")
+		console.log("Asked file=" + urlFile)
 		if (req.url == "/") {
+			var index = fs.readFileSync('index.html');
 			answer = index
-		} else if (req.url == "/page2") {
-			if (!homeTem) {
-				homeTemplate = fs.readFileSync("page2.html");
-			};
+		} else if (urlFile == URLS["page2"]) {
+			homeTemplate = fs.readFileSync("page2.html");
 			answer = homeTemplate
-		};
+		} else if (urlFile == URLS["SSE"]) {
+			setInterval(function () {
+				constructSSE(res, id, )
+			})
+		} else {
+			answer = fs.readFileSync("." + urlFile)
+		}
 		
 		// answer += "URL=" + req.url + "\n"
 		// answer += "Headers=" + JSON.stringify(req.headers, null, 2) + "\n"
