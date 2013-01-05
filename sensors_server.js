c3d4ec7398a408714f962c75c4d38ae69d6ceafd
Troll <trolldu26@gmail.com>
2013-01-05 17:22:57 +0100

    Found a way to deal with those 14-bytes-long frames... still need to be tested

--- sensors_server.js
+++ sensors_server.js
@@ -1,58 +1,32 @@
 //* Server that deals with the data from the sensors
 
 function start (db, web_serv) {
+	FRAME_SIZE = 14
+	frames = []
 	console.log("Starting Sensors server")
 	var net = require("net");
 	var server = net.createServer(function(stream) {
-		var chatter = new Chatter(stream);
-		chatters.push(chatter);
+		// var chatter = new Chatter(stream);
+		// chatters.push(chatter);
 
 		stream.setTimeout(0);
 		stream.setEncoding("utf8");
 
 		stream.addListener("connect", function(){
-			stream.write("Hallo, wer bist du?:\n");
+			console.log("New sensors server connection established.")
 		});
 
-		var buffer = "";
+		var buffer = new Buffer("");
 		stream.addListener("data", function (data) {
-			if(chatter.name == null) {
-				buffer += data;
-				if (buffer.indexOf('\n') == -1) return;
-				chatter.name = buffer.match(/\S+/);
-				stream.write("....................\n");
-				chatters.forEach(function(c){
-					if (c != chatter) {
-						c.stream.write(chatter.name + " ist dem Chat beigetreten!\n");
-					}
-				});
-				return;
-			}
-
-			var command = data.match(/^\/(.*)/);
-			if (command) {
-				if (command[1] == 'users') {
-					chatters.forEach(function(c) {
-						stream.write("- " + c.name + "\n");
-					});
-				}
-				else if (command[1] == 'quit') {
-					stream.end();
-				}
-			}
-
-			chatters.forEach(function(c) {
-				if(c != chatter) {
-					c.stream.write(chatter.name + ": " + data);
-				}
-			});
+			buffer.concat([new Buffer(data)])
+			while (buffer.length >= FRAME_SIZE) {//* We have at least complete frame (14 bytes)
+				frames.push(buffer.slice(0, FRAME_SIZE)) //* Pushes the new "complete" frame with the other ones
+				buffer = buffer.slice(FRAME_SIZE) //* Crops the current buffer 
+			};
 		});
 
 		stream.addListener("end", function(){
-			chatters.remove(chatter);
-			chatters.forEach(function(c) {
-				c.stream.write(chatter.name + " hat den Chat verlassen.\n");
-			});
+			console.log("Closing a sensors server connection")
 
 			stream.end();
 		});
@@ -60,3 +34,5 @@ function start (db, web_serv) {
 
 	server.listen(8000);
 }
+
+exports.start = start
