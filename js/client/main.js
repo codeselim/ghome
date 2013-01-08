b0c53f6f148343bde70e6578835d2482f06e1e6a
Nicolas Dossou-Gbété <ndossougbe@mail>
2013-01-08 11:19:29 +0100

    JS is now correctly run when switching through different single-page html files

--- js/client/main.js
+++ js/client/main.js
@@ -1,31 +1,63 @@
 require.config({ 
-    baseUrl: "js/client"
-  , paths: {  
-   "jquerymobile": "../vendor/jquery.mobile-1.2.0.min"
-  }
-  , shim : {
-    "jquerymobile" : ['jquery']
-  }
+	baseUrl: "js/client"
+	, paths: {  
+		"jquerymobile": "../vendor/jquery.mobile-1.2.0.min"
+	}
+	, shim : {
+		"jquerymobile" : ['jquery']
+	}
 })
 
 
 require(["jquery", "sseListener", "jquerymobile"], function($,sseListener) {
-    $(function() {
-        $('body').css('visibility', 'visible')
-        console.log($)
-        console.log(sseListener)
+	$(function() {
+		$('body').css('visibility', 'visible')
 
-        var onSSERecieved = function(e) {
-          $('#sseOutput').append("<br />" + e.data)
-        }
+		$( '#home' ).live( 'pageinit',function(event){
+			console.log('home live')
+		});
+		$( '#notif' ).live( 'pageinit',function(event){
+			console.log('notif live')
+		});
 
-        $('#sseToggle').change(function() { 
-          if ($(this).val() == 'on') {
-            sseListener.enableSSE(onSSERecieved)
-          } else {
-            sseListener.disableSSE()
-            $('#sseOutput').html('')
-          }
-        })
-    })
+		var homePI = function() {
+			console.log('pageinit home!')
+			$('#btn1').click(function(){
+				console.log('btn1')
+				$('#mmc').removeAttr('hidden')
+			})} 
+
+			var notifPI = function() {
+				console.log('pageinit notif!')
+				var onSSERecieved = function(e) {
+					$('#sseOutput').append("<br />" + e.data)
+				}
+
+				$('#sseToggle').change(function() { 
+					console.log('toggled!')
+					if ($(this).val() == 'on') {
+						sseListener.enableSSE(onSSERecieved)
+					} else {
+						sseListener.disableSSE()
+						$('#sseOutput').html('')
+					}
+				})
+			}
+
+			//* Registering the page inits
+			pageinits = {
+				"home" : homePI,
+				"notif" : notifPI
+			}
+
+			for( id in pageinits) {
+				$(document).delegate("#" + id, "pageinit", pageinits[id])	
+			}
+
+			//* When the page is loaded from non AJAX request, pageinit event
+			//* is fired before the functions are bound. It has to be called
+			//* here.
+			pageinits[$('[data-role="page"]:first').attr('id')]()
+
+		})
 })
