4768eb7c1a398dfec776b809342889e6ed6badbe
td <nothing@nothing.com>
2013-01-13 16:56:54 +0100

    working simple server

---
+++ js/server/test_android_service_server.js
@@ -0,0 +1,17 @@
+var net = require("net");
+console.log("Starting Android Service server")
+var server = net.createServer(function(stream) {
+	stream.setTimeout(0);
+	stream.setEncoding("utf8");
+
+	stream.addListener("connect", function(){
+		console.log("New server connection established.")
+	});
+
+	var buffer = ""
+	stream.addListener("data", function (data) {
+		console.log(new Date(), "New data packet came in:", data)
+	});
+})
+
+server.listen(5000, "192.168.0.13");
