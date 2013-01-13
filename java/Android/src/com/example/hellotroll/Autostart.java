843ab4aba23adb7e5155b79f7ac500aba1340511
td <nothing@nothing.com>
2013-01-14 00:04:17 +0100

    Android service is now run at the startup of Android

---
+++ java/Android/src/com/example/hellotroll/Autostart.java
@@ -0,0 +1,16 @@
+package com.example.hellotroll;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class Autostart extends BroadcastReceiver 
+{
+    public void onReceive(Context arg0, Intent arg1) 
+    {
+        Intent intent = new Intent(arg0, TrollService.class);
+        arg0.startService(intent);
+        Log.i("Autostart", "started");
+    }
+}
