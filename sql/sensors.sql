495718fec3d18c23163cdde0517846deecada13f
tdubourg <trolldu26@gmail.com>
2013-01-20 15:02:57 +0100

    Update sql/sensors.sql
    Mandatory fields => NOT NULL

--- sql/sensors.sql
+++ sql/sensors.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `sensors` (
   `id` INTEGER PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
-  `sensor_type_id` VINTEGER,
+  `sensor_type_id` VINTEGER NOT NULL,
    FOREIGN KEY(sensor_type_id) REFERENCES sensors_types(id)
 );
