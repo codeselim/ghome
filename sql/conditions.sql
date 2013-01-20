c5c7db342b3327def4f031901629b427621fea16
tdubourg <trolldu26@gmail.com>
2013-01-20 15:01:31 +0100

    Update sql/conditions.sql
    Mandatory fields => NOT NULL

--- sql/conditions.sql
+++ sql/conditions.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `conditions` (
   `id` INTEGER PRIMARY KEY,
   `value_to_compare` VARCHAR(255) NOT NULL,
-  `event_type_id` INTEGER,
-  `task_id` INTEGER,
-  `type_id` INTEGER,
-  `sensor_id` INTEGER,
+  `event_type_id` INTEGER NOT NULL,
+  `task_id` INTEGER NOT NULL,
+  `type_id` INTEGER NOT NULL,
+  `sensor_id` INTEGER NOT NULL,
 
   FOREIGN KEY(event_type_id) REFERENCES event_types(id),
   FOREIGN KEY(task_id) REFERENCES tasks(id),
