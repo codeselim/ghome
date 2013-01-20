29010e68cdcde1f68fd61ca4c431087e70690634
tdubourg <trolldu26@gmail.com>
2013-01-20 14:58:22 +0100

    Update sql/tasks.sql
    Mandatory values have to be NOT NULL.
    The "hour" in the day is actually not a "TIME" by itself.
    The "Date" data type will store the year and we just want to be able to compare a day in the year (but, without problems of 365 vs 366-days-long years)

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `tasks` (
   `id` INTEGER PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
-  `action_type_id` INTEGER,
-  `target_id` INTEGER,
-  `mode_id` INTEGER,
-  `max_date` DATE,
-  `min_date` DATE,
-  `max_hour` TIME,
-  `min_hour` TIME,
+  `action_type_id` INTEGER NOT NULL,
+  `target_id` INTEGER NOT NULL,
+  `mode_id` INTEGER NOT NULL, -- still work to be done on that : do we allow multiple modes? Thus pushing that into the `conditions` table?
+  `max_date` CHAR(5) NOT NULL, -- Value of type 'MM-DD'
+  `min_date` CHAR(5) NOT NULL, -- Value of type 'MM-DD'
+  `max_hour` INTEGER UNSIGNED NOT NULL, -- 0 <= h < 24
+  `min_hour` INTEGER UNSIGNED NOT NULL, -- 0 <= h < 24
   FOREIGN KEY(action_type_id) REFERENCES actions_types(id),
   FOREIGN KEY(target_id) REFERENCES sensors(id),
   FOREIGN KEY(mode_id) REFERENCES modes(id)
