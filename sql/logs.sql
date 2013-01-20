f14011e16950b0d8a6e536fe8468a4a3138c99a7
tdubourg <trolldu26@gmail.com>
2013-01-20 15:02:22 +0100

    Update sql/logs.sql
    Mandatory fields => NOT NULL

--- sql/logs.sql
+++ sql/logs.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `logs` (
   `id` INTEGER PRIMARY KEY,
   `sensor_id` INTEGER NOT NULL, 
-  `value` VARCHAR(255),
-  `time` DATE,
+  `value` VARCHAR(255) NOT NULL,
+  `time` TIME NOT NULL,
    FOREIGN KEY(sensor_id) REFERENCES sensors(id)
 );
