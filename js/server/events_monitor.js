ac9d6ed00f1ffefc6d6f5dfbd6e4367be4b95b6d
Julien Le Bras <julien.le-bras@insa-lyon.fr>
2013-01-22 11:52:34 +0100

    Add hardware_id for sensors and update events_monitor.js

--- js/server/events_monitor.js
+++ js/server/events_monitor.js
@@ -1,8 +1,25 @@
 //* Gets data parsed from sensors and check if it's associated to a real event
+//* from the database
+var db;
+var eventTypeId;
+var eventToSend;
+function start(database) {
+	db = database;
+}
 
-function getEvent(idSensor, typeSensor, dataSensor) {
+function setEventTypeId(err, rows) {
+	 for (var r in rows) {
+      console.log(r);
+  } 
+}
 
+function getData(idSensor, dataSensor) {
+
+/*eventToSend["data"] = dataSensor;*/
+db.query("SELECT * FROM sensors, sensors_types WHERE ? = ? AND ? = ?", ["sensor_type_id","sensors_types.id", "sensors.id", idSensor], setEventTypeId);
 
 }
 
-exports.getEvent = getEvent
+exports.getData = getData
+
+getData(36, 48);
--- sql/sensors.sql
+++ sql/sensors.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `sensors` (
   `id` INTEGER PRIMARY KEY,
+  `hardware_id` INTEGER NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  `sensor_type_id` VINTEGER NOT NULL,
+  `sensor_type_id` INTEGER NOT NULL,
    FOREIGN KEY(sensor_type_id) REFERENCES sensors_types(id)
 );
