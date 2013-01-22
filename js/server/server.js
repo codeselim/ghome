bcf55d69ce1efae302823fd81222b974e3d6fa21
tdubourg <plop@plop.fake>
2013-01-22 10:06:41 +0100

    DBMS Class now in place

--- js/server/server.js
+++ js/server/server.js
@@ -9,7 +9,7 @@ var shared = require('./shared_data')
 var sse_sender = require('./sse_sender')
 var get_shared_data = shared.get_shared_data
 var set_shared_data = shared.set_shared_data
-
+var dbms = require('./dbms')
 
 var cp = require('child_process')
 var n = cp.fork(__dirname + '/background_worker.js')
@@ -40,19 +40,25 @@ function frame_to_android_notif (frame_data) {
  * It will for instance get the last inside/outised temperatures and push them into memory, etc. .. 
  * It will, among other things, bring the server to the state it was when it was shutdown (either gracefully or suddenly..)
  */
+ var db = null
 function GLOBAL_INIT () {
 	set_shared_data('IN_TEMP', 0) // @TODO : Get the value from the database instead !
 	set_shared_data('OUT_TEMP', -2) // @TODO : Get the value from the database instead !
 	set_shared_data('MAIN_SERVER_IP', '134.214.105.28')
 	set_shared_data('MAIN_SERVER_PORT', 5000)
+	db = new dbms.Database()
+	db.connect('dat', start)
 }
 
-//@TODO : Find a way to organize the packages so that they share the data
+function start () {
+	console.log('Database connected, starting server components.')
+	web_serv.start(db, 9615)
+	android_notif_serv.start(5000, "0.0.0.0") // DO NOT CHANGE THIS PORT NUMBER (Well, or test after changing it !) I don't know why, but it's working on port 5000 and not on port 3000 for instance....
+	sensors_serv.events.addListener(sensors_serv.SENSOR_FRAME_EVENT, frame_processor)
+	sensors_serv.events.addListener(sensors_serv.SENSOR_FRAME_EVENT, sse_sender.sendSSE)
+	sensors_serv.events.addListener(sensors_serv.SENSOR_FRAME_EVENT, frame_to_android_notif)
+	var allowed_ids = [2214883, 346751, 6] //  @TODO : Put ALL OF THE IDS here // Note : The "6" is for debugging, remove before production
+	sensors_serv.start(db, web_serv, 8000, allowed_ids)
+}
+
 GLOBAL_INIT()
-web_serv.start(null, 9615)
-android_notif_serv.start(5000, "0.0.0.0") // DO NOT CHANGE THIS PORT NUMBER (Well, or test after changing it !) I don't know why, but it's working on port 5000 and not on port 3000 for instance....
-sensors_serv.events.addListener(sensors_serv.SENSOR_FRAME_EVENT, frame_processor)
-sensors_serv.events.addListener(sensors_serv.SENSOR_FRAME_EVENT, sse_sender.sendSSE)
-sensors_serv.events.addListener(sensors_serv.SENSOR_FRAME_EVENT, frame_to_android_notif)
-var allowed_ids = [2214883, 346751, 6] //  @TODO : Put ALL OF THE IDS here // Note : The "6" is for debugging, remove before production
-sensors_serv.start(null, null, 8000, allowed_ids)
