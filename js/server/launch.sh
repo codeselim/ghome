94662a476ce0dc55e718b32db64be699b41a6107
tdubourg <plop@plop.fake>
2013-01-25 12:03:02 +0100

    Launching and killing things the right way! Even root ones

--- js/server/launch.sh
+++ js/server/launch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Just a trick so that we can launch node server.js with the "&" without sudo giving up
-sudo su -c 'node server.js &' && node redirect.js &
+sudo echo "Fuck sudo" > /dev/null # Does not do anything, necessary to get sudo OK
+sudo su -c 'node server.js & echo $! > ./main_server.pid && chmod 0777 -v ./main_server.pid'
+node redirect.js & # Actually launches things
+echo $! > ./redirect.pid
