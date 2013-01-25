73878e35516d02ad7ddd0dfa020194ea2e9662c8
tdubourg <plop@plop.fake>
2013-01-25 10:42:53 +0100

    Cleaning code of redirect.js + handling reconnection to local server so that we can keep the connection to main sensors server alive when restarting our local server

--- js/server/redirect.js
+++ js/server/redirect.js
@@ -3,14 +3,23 @@ var sock
 function connectRemote() {
     sock = net.connect(5000, '134.214.105.28', function () { console.log("Connection to broadcast server estblished")})
 	sock.on("data", redirect);
-	sock.on("end", function () { console.log("Connection ended, trying to reconnect in 10s"); setTimeout(connectRemote, 10000);})
+	sock.on("end", function () { console.log("REMOTE connection ENDED, trying to reconnect in 10s"); setTimeout(connectRemote, 10000);})
+	sock.on("error", function () { console.log("REMOTE connection ERROR, trying to reconnect in 5s"); setTimeout(connectRemote, 5000);})
 }
 
-var localhost = new net.Socket()
-localhost.connect(8000, "localhost", function () { console.log("Connection to localhost server estblished") })
+
+var localhost = null
+
+function connectLocal (argument) {
+	localhost = new net.Socket()
+	localhost.connect(8000, "localhost", function () { console.log("Connection to localhost server estblished") })
+	localhost.on("end", function () { console.log("LOCAL connection ENDED, trying to reconnect in 1s"); setTimeout(connectLocal, 1000);})
+	localhost.on("error", function () { console.log("LOCAL connection ERROR, trying to reconnect in 1s"); setTimeout(connectLocal, 1000);})
+}
 
 function redirect(data) {
 	localhost.write(data);
 }
 
+connectLocal()
 connectRemote();
