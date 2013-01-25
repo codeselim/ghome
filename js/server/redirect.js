4e123d0a2aaeaf89b77462eda015ee5ceaa26353
tdubourg <plop@plop.fake>
2013-01-25 11:49:26 +0100

    code cleaning

--- js/server/redirect.js
+++ js/server/redirect.js
@@ -1,7 +1,7 @@
 var net = require('net')
 var sock
 function connectRemote() {
-    sock = net.connect(5000, '134.214.105.28', function () { console.log("Connection to broadcast server estblished")})
+	sock = net.connect(5000, '134.214.105.28', function () { console.log("Connection to broadcast server estblished")})
 	sock.on("data", redirect);
 	sock.on("end", function () { console.log("REMOTE connection ENDED, trying to reconnect in 10s"); setTimeout(connectRemote, 10000);})
 	sock.on("error", function () { console.log("REMOTE connection ERROR, trying to reconnect in 5s"); setTimeout(connectRemote, 5000);})
@@ -9,16 +9,18 @@ function connectRemote() {
 
 
 var localhost = null
-
+var localIsConnected = false
 function connectLocal (argument) {
 	localhost = new net.Socket()
-	localhost.connect(8000, "localhost", function () { console.log("Connection to localhost server estblished") })
-	localhost.on("end", function () { console.log("LOCAL connection ENDED, trying to reconnect in 1s"); setTimeout(connectLocal, 1000);})
-	localhost.on("error", function () { console.log("LOCAL connection ERROR, trying to reconnect in 1s"); setTimeout(connectLocal, 1000);})
+	localhost.connect(8000, "localhost", function () { console.log("Connection to localhost server estblished"); localIsConnected = true})
+	localhost.on("end", function () { console.log("LOCAL connection ENDED, trying to reconnect in 1s"); setTimeout(connectLocal, 1000); localIsConnected = false})
+	localhost.on("error", function () { console.log("LOCAL connection ERROR, trying to reconnect in 1s"); setTimeout(connectLocal, 1000); localIsConnected = false})
 }
 
 function redirect(data) {
-	localhost.write(data);
+	if (localIsConnected) {
+		localhost.write(data);
+	};
 }
 
 connectLocal()
