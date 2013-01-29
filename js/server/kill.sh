9ee01885a6d4b3448422be1098c551eb9e09d403
tdubourg <plop@plop.fake>
2013-01-29 16:59:19 +0100

    Even smarter (without bugs,mainly :P)

--- js/server/kill.sh
+++ js/server/kill.sh
@@ -1,9 +1,9 @@
 case $1 in
 	main)
 		sudo echo "Blorg" > /dev/null 
-		sudo kill -TERM `cat ./main_server.pid` 2>/dev/null && rm -f ./main_server.pid;;
+		sudo kill -TERM `cat ./main_server.pid 2>/dev/null` 2>/dev/null && rm -f ./main_server.pid;;
 	redirect)
-		kill -TERM `cat ./redirect.pid` 2>/dev/null && rm -f ./redirect.pid;;
+		kill -TERM `cat ./redirect.pid 2>/dev/null` 2>/dev/null && rm -f ./redirect.pid;;
 	all)
 		a=`./kill.sh redirect` 
 		b=`./kill.sh main`;;
