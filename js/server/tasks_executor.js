d4076c35ac40c622e25c077117392afafa558307
Fhyren <fhyren@fhyren-G74Sx.(none)>
2013-01-29 10:55:56 +0100

    debug espaces

--- js/server/tasks_executor.js
+++ js/server/tasks_executor.js
@@ -1,4 +1,5 @@
 var db;
+
 function make_action(err, rows) {
 	 for (var r in rows) {
       console.log(rows[r]["action_type_id"]);
@@ -24,7 +25,7 @@ function execute_task(event_id, value) {
 	console.log(day.length)*/
 
 	db.query("SELECT action_type_id FROM Tasks AS t, conditions AS c, condition_types AS ct WHERE t.id = c.task_id AND ct.id = c.type_id AND event_type_id = ? AND " + month + " < max_month AND " + month + " > min_month AND "
-		+ day + " < max_day AND" + day + " > min_day AND " + hour + " < max_hour AND" + hour + " > min_hour"
+		+ day + " < max_day AND " + day + " > min_day AND " + hour + " < max_hour AND " + hour + " > min_hour"
 		, [event_id], make_action)
 }
 
