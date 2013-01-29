ec4223a7b4e3af27ff11c9bdaf6c61372ae9882e
tdubourg <plop@plop.fake>
2013-01-29 11:35:22 +0100

    Updating the launch.sh and kill.sh so that everything works and database is reset + test data added each time we launch

--- js/server/kill.sh
+++ js/server/kill.sh
@@ -1,10 +1,11 @@
 case $1 in
 	main)
-		sudo kill -TERM `cat ./main_server.pid` && rm -vf ./main_server.pid;;
+		sudo echo "Blorg" > /dev/null && 
+		sudo kill -TERM `cat ./main_server.pid` 2>/dev/null && rm -f ./main_server.pid;;
 	redirect)
-		kill -TERM `cat ./redirect.pid` && rm -vf ./redirect.pid;;
+		kill -TERM `cat ./redirect.pid` && rm -f ./redirect.pid;;
 	all)
-		sudo kill -TERM `cat ./main_server.pid` && rm -vf ./main_server.pid;
-		kill -TERM `cat ./redirect.pid` && rm -vf ./redirect.pid;;
+		a=`./kill.sh redirect` && 
+		b=`./kill.sh main`;;
 esac
 	
--- js/server/launch.sh
+++ js/server/launch.sh
@@ -3,15 +3,23 @@
 a=$1
 
 # echo "a="$a"blorg"
+origin=`pwd` # saving current dir
 
 case $a in
 	m)
-		sudo echo "Fuck sudo" > /dev/null &&  # Does not do anything, necessary to get sudo OK
+		echo "Resetting DB..." 
+		cd ../../sql/ && rm -vf dat.db && sqlite3 dat.db < dataBaseInit.txt && sqlite3 dat.db < sql_test_data.txt &&
+		cd $origin 
+		echo "Launching main server as root..." 
+		sudo echo "Fuck sudo" > /dev/null   # Does not do anything, necessary to get sudo OK
 		sudo su -c 'node server.js & echo $! > ./main_server.pid && chmod 0777 -v ./main_server.pid';;
 	r)
-		node redirect.js &	echo $! > ./redirect.pid;;
+		echo "Launching redirect server..."
+		node redirect.js & echo $! > ./redirect.pid;;
 	all)
-		./launch.sh m && ./launch.sh r;;
+		./launch.sh m
+		./launch.sh r;;
 	*)
+		echo "No parameters passed, considering parameter \"all\" "
 		./launch.sh all;;
 esac
