5398bba155512d86ea48546756b6616455dc96db
Fhyren <fhyren@fhyren-G74Sx.(none)>
2013-01-30 16:00:48 +0100

    refactoring task executor

--- js/server/tasks_executor.js
+++ js/server/tasks_executor.js
@@ -1,9 +1,26 @@
 var db;
+var sensors_server = require("./sensors_server.js");
 
-function make_action(err, rows) {
-	console.log("rentrée dans callback")
-	 for (var r in rows) {
-      console.log(rows[r]);
+function switch_on_plug(){
+	sensors_server.send_to_sensor(null, sensors_server.PLUG_SWITCH_ON_FRAME)
+}
+
+function switch_off_plug(){
+	sensors_server.send_to_sensor(null, sensors_server.PLUG_SWITCH_OFF_FRAME)
+}
+
+function make_action(results) {
+	 for (var r in results) {
+      switch(results[r]){
+      	case 0 :
+      	switch_on_plug();
+      	break;
+      	case 1 :
+      	switch_off_plug();
+      	break;
+      	default :
+      	break;
+      }
   } 
 }
 
@@ -11,17 +28,50 @@ function start (database){
 	db = database;
 }
 
-function execute_task(event_id, value) {
+function execute_task(event_id, value, sensor_id) {
 
 	var date= new Date()
 	var month = date.getMonth()
 	var day = date.getDay()
 	var hour = date.getHours()
-	console.log(event_id)
+	var results = new Array();
+	//console.log(event_id)
 
-	db.query("SELECT action_type_id FROM Tasks AS t, conditions AS c, condition_types AS ct WHERE t.id = c.task_id AND ct.id = c.type_id AND event_type_id = ? AND " + month + " <= max_month AND " + month + " >= min_month AND "
-		+ day + " <= max_day AND " + day + " >= min_day AND " + hour + " <= max_hour AND " + hour + " >= min_hour"
-		, [event_id], make_action)
+	//We get the action type id and the operator from the candidate actions (actions wich are in the right timer for being candidate)
+	db.query("SELECT action_type_id, operator, value_to_compare FROM Tasks AS t, conditions AS c, condition_types AS ct WHERE t.id = c.task_id AND ct.id = c.type_id AND event_type_id = ? AND " + month + " <= max_month AND " + month + " >= min_month AND "
+		+ day + " <= max_day AND " + day + " >= min_day AND " + hour + " <= max_hour AND " + hour + " >= min_hour AND sensor_id = ?"
+		, [event_id, sensor_id], function (err, rows) { //now we select the proper actions with the operator 
+			for (var r in rows){
+				switch (rows[r]["operator"]){
+				case 0 : // if operator = "="
+					if (rows[r]["value_to_compare"] == value){
+						results.push(rows[r]["action_type_id"]);
+					}
+					default;
+				case 1 :  // if operator = "<"
+					if (rows[r]["value_to_compare"] < value){
+						results.push(rows[r]["action_type_id"]);
+					}
+					default;
+				case 2 : // if operator = ">"
+					if (rows[r]["value_to_compare"] > value){
+						results.push(rows[r]["action_type_id"]);
+					}
+					default;
+				case 3 : // if operator = "<="
+					if (rows[r]["value_to_compare"] <= value){
+						results.push(rows[r]["action_type_id"]);
+					}
+					default;
+				case 4 : // if operator = ">="
+					if (rows[r]["value_to_compare"] >= value){
+						results.push(rows[r]["action_type_id"]);
+					}
+					default;
+				}
+			}
+			make_action(results);
+		})
 }
 
 exports.execute_task = execute_task
