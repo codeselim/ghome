d539c8ee53b61313eac6990c7c500bed3743de2d
Fhyren <fhyren@fhyren-G74Sx.(none)>
2013-01-30 16:31:32 +0100

    ajout d actions et d operators

--- js/server/tasks_executor.js
+++ js/server/tasks_executor.js
@@ -1,5 +1,6 @@
 var db;
 var sensors_server = require("./sensors_server.js");
+var sse_sender = require("./sse_sender.js");
 
 function switch_on_plug(){
 	sensors_server.send_to_sensor(null, sensors_server.PLUG_SWITCH_ON_FRAME)
@@ -9,16 +10,27 @@ function switch_off_plug(){
 	sensors_server.send_to_sensor(null, sensors_server.PLUG_SWITCH_OFF_FRAME)
 }
 
-function make_action(results) {
+function make_action(results) { //this function will make the actions of results
 	 for (var r in results) {
       switch(results[r]){
-      	case 0 :
-      	switch_on_plug();
-      	break;
       	case 1 :
+      	switch_on_plug();
+      	sse_sender.sendSSE({"msg" : "Prise allumée"});
+      	break;
+      	case 2 :
       	switch_off_plug();
+      	sse_sender.sendSSE({"msg" : "Prise éteinte"});
+      	break;
+      	case 3 :
+      	switch_off_plug();
+      	sse_sender.sendSSE({"msg" : "Volets ouverts"});
+      	break;
+      	case 4 :
+      	switch_off_plug();
+      	sse_sender.sendSSE({"msg" : "Volets fermés"});
       	break;
       	default :
+      	sse_sender.sendSSE({"msg" : "Rien du tout"});
       	break;
       }
   } 
@@ -28,7 +40,7 @@ function start (database){
 	db = database;
 }
 
-function execute_task(event_id, value, sensor_id) {
+function execute_task(event_id, value, sensor_id) {//this function will search the good actions to do and call make_action with the results in order to make the action effective
 
 	var date= new Date()
 	var month = date.getMonth()
@@ -43,31 +55,35 @@ function execute_task(event_id, value, sensor_id) {
 		, [event_id, sensor_id], function (err, rows) { //now we select the proper actions with the operator 
 			for (var r in rows){
 				switch (rows[r]["operator"]){
-				case 0 : // if operator = "="
+					case 1 : // if operator = "="
 					if (rows[r]["value_to_compare"] == value){
 						results.push(rows[r]["action_type_id"]);
 					}
-					default;
-				case 1 :  // if operator = "<"
+					break;
+					case 2 :  // if operator = "<"
 					if (rows[r]["value_to_compare"] < value){
 						results.push(rows[r]["action_type_id"]);
 					}
-					default;
-				case 2 : // if operator = ">"
+					break;
+					case 3 : // if operator = ">"
 					if (rows[r]["value_to_compare"] > value){
 						results.push(rows[r]["action_type_id"]);
 					}
-					default;
-				case 3 : // if operator = "<="
+					break;
+					case 4 : // if operator = "<="
+					case 7 : // if operator = "passage de seuil bas"
 					if (rows[r]["value_to_compare"] <= value){
 						results.push(rows[r]["action_type_id"]);
 					}
-					default;
-				case 4 : // if operator = ">="
+					break;
+					case 5 : // if operator = ">="
+					case 6 : // if operator = "passage de seuil haut"
 					if (rows[r]["value_to_compare"] >= value){
 						results.push(rows[r]["action_type_id"]);
 					}
-					default;
+					break;
+					default :
+					break;
 				}
 			}
 			make_action(results);
