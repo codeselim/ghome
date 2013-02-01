b1aa35cfb4d716dc0e99af51353fec79c1f05ac9
selimabisaber <selimabisaber@gmail.com>
2013-02-01 17:34:29 +0100

    adjusted tables + test data :*

--- sql/actions_types.sql
+++ sql/actions_types.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `actions_types` (
   `id` INTEGER PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
-  `sensor_type_id` INTEGER
+  `sensor_type_id` INTEGER,
+  `message_to_sensor` VARCHAR(255)
 );
