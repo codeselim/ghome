caeafd09e8472b6bd601126b581ba840834c845e
atoumia <amri.toumia@insa-lyon.fr>
2013-02-05 11:40:50 +0100

    add min max in stat tables

--- sql/hour_stats.sql
+++ sql/hour_stats.sql
@@ -2,6 +2,8 @@ CREATE TABLE `hour_stats` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `sensor_type_id` INTEGER NOT NULL,
   `value` INTEGER NOT NULL,
+  `min` INTEGER ,
+  `max` INTEGER ,
   `time` DATE NOT NULL,
   FOREIGN KEY(sensor_type_id) REFERENCES sensor_types(id)
 );
--- sql/monthly_stats.sql
+++ sql/monthly_stats.sql
@@ -2,6 +2,8 @@ CREATE TABLE `monthly_stats` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `sensor_type_id` INTEGER NOT NULL,
   `value` INTEGER NOT NULL,
+  `min` INTEGER ,
+  `max` INTEGER ,
   `time` INTEGER NOT NULL,
   FOREIGN KEY(sensor_type_id) REFERENCES sensor_types(id)
 );
