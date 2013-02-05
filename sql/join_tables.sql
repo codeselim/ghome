989fac7d986111d8f73b07e5199e1262b1b6580b
tdubourg <plop@plop.fake>
2013-02-05 19:41:24 +0100

    Better with the actual file...

---
+++ sql/join_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `event_types_condition_types` (
+	`event_type_id` INT NOT NULL,
+	`condition_type_id` INT NOT NULL,
+	FOREIGN KEY(event_type_id) REFERENCES event_types(`id`),
+	FOREIGN KEY(condition_type_id) REFERENCES condition_types(`id`),
+	PRIMARY KEY(`event_type_id`, `condition_type_id`)
+);
+
+CREATE TABLE `sensor_types_event_types` (
+	`sensor_type_id` INT NOT NULL,
+	`event_type_id` INT NOT NULL,
+	FOREIGN KEY(sensor_type_id) REFERENCES sensors_types(`id`),
+	FOREIGN KEY(event_type_id) REFERENCES event_types(`id`),
+	PRIMARY KEY(`sensor_type_id`, `event_type_id`)
+);
+
+CREATE VIEW `sensor_types_condition_types` AS 
+	SELECT sensor_type_id, condition_type_id 
+	FROM event_types_condition_types etct 
+	INNER JOIN sensor_types_event_types stet ON (etct.event_type_id = stet.event_type_id);
+
+-- CREATE TABLE `event_type_condition_types` (
+-- 	`` INT NOT NULL,
+-- 	`` INT NOT NULL,
+-- 	PRIMARY KEY(``, ``)
+-- );
+
