d0e1efc3044851611cd2bd47c4c2a322304dda6f
Julien Le Bras <julien.le-bras@insa-lyon.fr>
2013-02-07 21:44:58 +0100

    Add spy_web_module.js and spy.html, main.js updated

--- js/client/main.js
+++ js/client/main.js
@@ -10,8 +10,8 @@ require.config({
 })
 
 
-require(['jquery', /*'prejqm',*/ 'sseListener', 'device_management', 'new_device', 'scheduler', 'jquerymobile'], 
-	function($, /*_,*/ sseListener, devMgmt, new_device, scheduler) {
+require(['jquery', /*'prejqm',*/ 'sseListener', 'device_management', 'new_device', 'scheduler', 'utils', 'jquerymobile'], 
+	function($, /*_,*/ sseListener, devMgmt, new_device, scheduler, utils) {
 	$(function() {
 
 		//* Hides the body until JQM finishes applying styles
@@ -48,7 +48,56 @@ require(['jquery', /*'prejqm',*/ 'sseListener', 'device_management', 'new_device
 			, 'task'   : scheduler.taskPageInit
 			, 'spy'       : function() {
 				// TODO : get recent logs from spy table and create a table with them
-				console.log("SPY FUNCTION INIT");
+				utils.initMessages();
+					var submitForm = function submitForm() {
+
+		var data = utils.queryStringToHash($.param($('input:not([type=button],[type=submit]),select')))
+		console.log(data)
+		data.module = 'spy'
+
+		data.action = 'submit_parameters'
+		
+
+		$.ajax({
+				'url'      : "/"
+			, 'dataType' : 'json'
+			, 'data'     : data
+		})
+		.done(function(data) {
+			console.log(data)
+			if (data.success) {
+				utils.addMessage('success', 'TODO: retourner le nouvel id pour pouvoir passer en mode édition')
+				window.location.href = '/?module=spy'
+				// setTimeout('top.location.href = "/?module=scheduler"',2000)
+			} else {
+				utils.addMessage('error', 'Une erreur est survenue: ' + data.msg)
+			}
+		})
+		.fail(function(a,status) { utils.addMessage('error', "Le formulaire n'a pas pu être envoyé") })
+	}
+
+						$("form").validate({
+			  rules: { 
+					  email: {required:true, email:true}
+				} 
+			, messages: { 
+					  email: {
+					  	required : "Email requis",
+					  	email : "Email non valide"
+					  }
+					
+				}
+			, errorPlacement: function(error, element) {
+				//* Needed to place the error message out of the select menu.
+				if (element.is('select')) {
+					error.insertAfter($(element).parent())
+				} else {
+					error.insertAfter(element)
+				}
+			}
+			, submitHandler: submitForm
+		})
+
 			}
 
 			
---
+++ js/server/spy_web_module.js
@@ -0,0 +1,63 @@
+"use strict"
+
+var fs = require('fs')
+var tpl = require('./template_engine')
+
+// 
+function errCallBack(err) {
+	if(err == null) {
+		console.log("Request went well")
+	}
+	else {
+		console.log("Request error")
+	}
+}
+
+var spyRequestHandler = function(req, res, params, responseSender) {
+
+	switch (params.query.action) {
+
+	case "submit_parameters":
+	console.log(params);
+	console.log("switchSpyMode :" + params.query.switchSpyMode)
+	// Can't use db.insert_query or select_query check with Théo
+	params.db.db.run("UPDATE settings SET value = ? WHERE name = ?", [params.query.switchSpyMode.toUpperCase(), "mode spy"], function(err) {
+		if (err != null) {
+			console.error("spyRequestHandler: Error when updating the spy mode value.", err)
+			res.end(JSON.stringify({'msg': JSON.stringify(err), 'success': false}))
+		}
+		else {
+		params.db.db.run("UPDATE settings SET value = ? WHERE name = ?", [params.query.email, "mail d envoi"], function(err) {
+			if (err != null) {
+				console.error("spyRequestHandler: Error when updating the mailing address.", err)
+				res.end(JSON.stringify({'msg': JSON.stringify(err), 'success': false}))
+			}
+			else {
+				res.end(JSON.stringify({'success': true}))
+			}
+		} )	
+		}
+	} )
+	
+	
+	break;
+
+	default:
+	
+	params.db.select_query("SELECT sensors.name, sensors_types.name AS stName, logs_spy.value, logs_spy.time " +
+		"FROM logs_spy JOIN sensors ON logs_spy.sensor_id = sensors.id JOIN sensors_types ON " +
+		"sensors.sensor_type_id = sensors_types.id  " +
+		"ORDER BY logs_spy.time LIMIT 100",[], function(err, rows) {
+			var tplData = {rows : rows};
+				//var tplData = {toto : "toto"}
+				var data = tpl.get_template_result("spy.html", tplData)
+				params.fileUrl = 'spy.html'
+				responseSender(req, res, params, data)
+
+		}  )
+
+	}
+
+}
+
+exports.spyRequestHandler = spyRequestHandler
