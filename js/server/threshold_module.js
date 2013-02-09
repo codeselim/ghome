79302e442aa037b5d56298eee83f8dc995aa3eb7
Julien Le Bras <julien.le-bras@insa-lyon.fr>
2013-02-09 19:13:35 +0100

    Add new column in sensors_types : thresholdable, update threshold_module and test data

--- js/server/threshold_module.js
+++ js/server/threshold_module.js
@@ -4,6 +4,7 @@
 var tpl = require('./template_engine')
 
 
+
 var editRH = function (req, res, params, responseSender) {
 	var data = {}
 	switch (params.query.action) {
@@ -17,14 +18,23 @@ var editRH = function (req, res, params, responseSender) {
 
 		case 'edit':
 			// break
+			// Pareil que new sauf vérif id seuil
 
 		case 'new':
 			params.fileUrl = 'threshold.html'
+			// liste types de capteurs
 			data = {
 				  'threshold' : {'id':1, 'label': 'Seuil1', 'value': 1234}
-				, 'sensors' : [{'id':1, 'label': 'sensor1'}, {'id':2, 'label': 'sensor2', 'selected':true}]
+				//, 'sensors' : [{'id':1, 'label': 'sensor1'}, {'id':2, 'label': 'sensor2', 'selected':true}]
 			}
-			responseSender(req, res, params, tpl.get_template_result("threshold.html", data))
+			params.db.select_query("SELECT sensors.id AS id, events_launchers_view.name "+
+				" AS sensorName FROM events_launchers_view JOIN sensors_types  ON "+
+				"events_launchers_view.sensor_type_id = sensors_types.id JOIN sensors ON "+
+				"events_launchers_view.hardware_id = sensors.hardware_id WHERE thresholdable = 1", [], function (err, rows) {
+				data.sensors = rows
+				responseSender(req, res, params, tpl.get_template_result("threshold.html", data))
+			})
+		
 			break
 		
 
@@ -40,11 +50,16 @@ var editRH = function (req, res, params, responseSender) {
 
 
 var listRH  = function (req, res, params, responseSender) {
-	var data = tpl.get_template_result("threshold_list.html", {
-		'thresholds' : [{'id':1, 'label':'seuil1', 'value': 1},{'id':2, 'label':'seuil2', 'value': 2}]
+	params.db.select_query("SELECT id, thresholdName, value FROM thresholds", [], function(err, rows) {
+		var data = tpl.get_template_result("threshold_list.html", {
+		//liste des seuils
+		'thresholds' : rows
 	})
 	params.fileUrl = 'threshold_list.html'
 	responseSender(req, res, params, data)
+
+	})
+
 }
 
 
--- sql/sensors_types.sql
+++ sql/sensors_types.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `sensors_types` (
   `id` INTEGER PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL
+  `name` VARCHAR(255) NOT NULL,
+  `thresholdable` INT(1) NOT NULL
 );
 
