233ce88b5fa01337b0cb0c62fcd4666c4d31c9b5
selimabisaber <selimabisaber@gmail.com>
2013-02-09 18:54:03 +0100

    error fixed

--- js/server/stats_computer.js
+++ js/server/stats_computer.js
@@ -1,4 +1,3 @@
-var db ;
 
 var tpl = require('./template_engine')
 var shared = require('./shared_data')
@@ -6,44 +5,86 @@ var SQL_TABLES_DIC = shared.get_shared_data('SQL_TABLES');
 
 
 
-
 function statsRH(req, res, params, responseSender){
-		var temp = {
-					'chart': {
-                 			'renderTo': "container",
-                 			'type': "line",
-                 			'marginRight': 130,
-                 			'marginBottom': 25
-             			},
-             		'title': {
-             			    'text': 'Température',
-                			'x': -20 //center
-             			},
-             		'xAxis': {
-                	'categories': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            			},
-					}
-
-		var templateData = {
-		// 'IN_TEMP'		       : shared.get_shared_data('IN_TEMP')
-		// , 'OUT_TEMP'	     : shared.get_shared_data('OUT_TEMP')
-		// , 'TEST_DATA'		 : params.postData
-		// , 'COLOR_TEMP_IN'  : temp2color(shared.get_shared_data('IN_TEMP'))
-		// , 'COLOR_TEMP_OUT' : temp2color(shared.get_shared_data('OUT_TEMP'))
+	//var d1 = new Date(date1);
+ 	//var d2 = new Date(date2);
+ 	//var diff = d2 - d1;
+ 	// var table = "";
+ 	// if (diff < 172800000 ) //difference between date2 and date1 is less than 2 days
+		// table = "hour_stats"
+ 	// else if (diff > 172800000 && diff < 3888000000 ) //difference between date2 and date1 is more than 2 days and less than 45 days
+		// table = "daily_stats"
+ 	// else 
+		// table = "monthly_stats"
+ 
+ 	var query_str = " select time, value,min, max "+
+					" from hour_stats "+
+					" where time between '2013-02-08 00:00' and '2013-02-09 22:00'  "+
+					" and sensor_type_id = 1; "	
+	params.db.select_query (query_str, null, 
+		function (err, rows){
+			var stats_data = {'chart': {
+                 				'renderTo': "container",
+                 				'type': "line",
+                 				'marginRight': 130,
+                 				'marginBottom': 25
+             				},
+             			'title': {
+             				    'text': 'Température',
+                				'x': -20 //center
+             				},
+             			'xAxis': {
+                		'categories': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            				},            			
+            			'yAxis': {
+        	        				'title': {
+            	        			'text': 'Temperature (°C)'
+ 		               				},
+                					'plotLines': [{
+                    				'value': 0,
+                    				'width': 1,
+                    				'color': '#808080'
+                					}]
+            				},
+            			 'tooltip': {
+                				'formatter': function() {  return '<b>'+this.series.name +'</b><br/>'+this.x+': '+this.y+'°C';   }
+            				},
+            			'legend': {	
+                				'layout': 'vertical',
+                				'align': 'right',
+                				'verticalAlign': 'top',
+                				'x': -10,
+                				'y': 100,
+                				'borderWidth': 0
+            				},
+           				'series': [{
+                				'name': 'Maximum',
+                				'data': [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6] 
+                					}, {
+                				'name': 'Moyenne',
+                				'data': [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+            						}, {
+                				'name': 'Minimum',
+                				'data': [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+            					}]
+            		}
+		
+			var templateData = {
+			// 'IN_TEMP'		       : shared.get_shared_data('IN_TEMP')
+			// , 'OUT_TEMP'	     : shared.get_shared_data('OUT_TEMP')
+			// , 'TEST_DATA'		 : params.postData
+			// , 'COLOR_TEMP_IN'  : temp2color(shared.get_shared_data('IN_TEMP'))
+			// , 'COLOR_TEMP_OUT' : temp2color(shared.get_shared_data('OUT_TEMP'))
 			//STATS_DATA :"chart: { renderTo: 'container', type: 'line', marginRight: 130, marginBottom: 25 },"
-			STATS_DATA :  JSON.stringify(temp)
-
+			STATS_DATA :  JSON.stringify(stats_data)
 			//JSON.parse('{ "chart": {    "renderTo": "container",  "type": "line", "marginRight": 130,  "marginBottom": 25 }}')
-		}
-
-console.log(params)
-
-	var data = tpl.get_template_result("stats.html", templateData)
-	console.log(params['pathname'])
-	params['fileUrl'] = 'stats.html'
-	responseSender(req, res, params, data)
-}
+			}
+			var data = tpl.get_template_result("stats.html", templateData)
+			console.log(params['pathname'])
+			params['fileUrl'] = 'stats.html'
+			responseSender(req, res, params, data)
+         })  //end call back
+} //END RH
 
 
 
@@ -178,24 +219,9 @@ console.log(params)
  *@params {string} type_sensor, date1, date2
  *@returns
 */
- function get_temperature_stats (type_sensor, date1, date2) {
- 	var d1 = new Date(date1);
- 	var d2 = new Date(date2);
- 	var diff = d2 - d1;
- 	var table = "";
- 	if (diff < 172800000 ) //difference between date2 and date1 is less than 2 days
-		table = "hour_stats"
- 	else if (diff > 172800000 && diff < 3888000000 ) //difference between date2 and date1 is more than 2 days and less than 45 days
-		table = "daily_stats"
- 	else 
-		table = "monthly_stats"
- 
- 	var query_str = " select time, value,min, max "+
-					"from ?"+
-					"where time between '?' and '?'"+
-					"and sensor_type_id = 1; "	
-	db.query (query_str, [table,date1,date2,type_sensor], getData (err, rows))
- }
+ function get_temperature_stats (date1, date2) {
+ 	
+ } //end get_temperature_stats 
 
 /**
  * Gets electricity stats for a certain type of sensor in [date1,date2]
@@ -215,10 +241,10 @@ console.log(params)
  	else 
 		table = "monthly_stats"
 
- 	var query_str = " select time, value "+
-					"from ?"+
-					"where time between '?' and '?'"+
-					"and sensor_type_id = 5; "	
+ 	var query_str = "select time, value "+
+					" from ? "+
+					" where time between '?' and '?' "+
+					" and sensor_type_id = 5; "	
 	db.query (query_str, [table,date1,date2,type_sensor], getData (err, rows))
 	if (err != NULL)
 		return rows
@@ -227,13 +253,13 @@ console.log(params)
  }
 
 
-function getData ( err, rows){
-	var array = [] ;
-	for (var r in rows) {
-    	//console.log(r.value);
-    	array.push(rows[r]);
-    }
-}
+// function getData ( err, rows){
+// 	var array = [] ;
+// 	for (var r in rows) {
+//     	//console.log(r.value);
+//     	array.push(rows[r]);
+//     }
+// }
 
 
 function start ( ) {
