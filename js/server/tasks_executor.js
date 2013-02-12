e870c4b84a85db433d7d9d685ed6616d4e2715d3
Fhyren <fhyren@fhyren-G74Sx.(none)>
2013-02-12 11:25:23 +0100

    test stats

--- js/server/tasks_executor.js
+++ js/server/tasks_executor.js
@@ -9,10 +9,8 @@ var stats_computer = require('./stats_computer.js')
 
 function send_message(target, action){
 	db.select_query("SELECT message_to_sensor FROM actions_types WHERE id = ?", [action], function (err, rows) {
-		console.log("YOUPI")
 		console.log("target :", target, "action :", action)
 		for (var r in rows){
-			console.log("YOUPISUPER")
 			device_communicator.sendToSensor (target, rows[r]["message_to_sensor"]);
 		}
 	})
@@ -41,14 +39,15 @@ function execute_task(event_id, origin_id) {//this function will search the good
 	var current_action = null;
 	var current_sensor_id = null;
 	var current_condition_id = null;
-	console.log("event id :")
+	console.log("-----------------------------event id :-------------------------------------")
 	console.log(event_id)
 	console.log("origin_id", origin_id)
 
 	//We get the action type id, the operator, the value to compare, the sensor_id and the target_id from the candidate actions (actions wich are in the right timer for being candidate)
 	db.select_query("SELECT action_type_id, operator, value_to_compare, sensor_id, target_id, c.id FROM Tasks AS t LEFT OUTER JOIN conditions AS c ON c.task_id = t.id LEFT OUTER JOIN condition_types AS ct ON ct.id = c.type_id WHERE event_type_id = ? AND origin_id = ?"
 			, [event_id, origin_id], function (err, rows) { //now we select the proper actions with the operator
-				/*if(event_id == 5){//change of day
+
+				if(event_id == 5){//change of day
 					stats_computer.temperature_d();
 					stats_computer.consumption_d();
 				} else if (event_id == 6) {//change of hour
@@ -57,7 +56,7 @@ function execute_task(event_id, origin_id) {//this function will search the good
 				} else if (event_id == 7) {//change of minute
 					stats_computer.temperature_m();
 					stats_computer.consumption_m();
-				}*/
+				}
 
 				for (var r in rows) { //creation of a dictionnaire where we put all the candidate actions
 					actions_type[rows[r]["action_type_id"]] = true;
@@ -92,7 +91,6 @@ function execute_task(event_id, origin_id) {//this function will search the good
 						}
 						break;
 						case 5 : // if operator = ">="
-						console.log("check >=")
 						if (parseInt(rows[r]["value_to_compare"]) < parseInt(value)){ //if we have the contrary of the operator, that means that the action have at least one condition wich is not respected, and we can't execute the action
 							actions_type[rows[r]["action_type_id"]] = false; //so we put the corresponding value to false = not executable
 						}
@@ -119,7 +117,6 @@ function execute_task(event_id, origin_id) {//this function will search the good
 						//@TODO : s'assurer que les rentrées coté client sont en phase avec les choix du code (si le nombre du mois corespond etc)
 
 						case 10 : //if operator is mois egal
-						console.log("check mois")
 						if (parseInt(rows[r]["value_to_compare"]) != (date.getMonth() + 1)){
 							actions_type[rows[r]["action_type_id"]] = false; //so we put the corresponding value to false = not executable
 						}
