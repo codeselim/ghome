83a826897bb9c5c9c335841735493370036c96cf
tdubourg <plop@plop.fake>
2013-02-12 10:27:57 +0100

    Now loading the main temperature sensors at the startup and also loading the allowd_ids at startup

--- js/server/server.js
+++ js/server/server.js
@@ -26,12 +26,12 @@ set_shared_data('SQL_TABLES', {'st': 'sensors_types',
 								'stct':'sensor_types_condition_types',
 								'thst':'thresholds_sensor_types'
 							})
-var allowed_ids = [8991608, 346751, 8991608, 111198, 112022, 115002] //  @TODO : Put ALL OF THE IDS here // Note : The "6" is for debugging, remove before production
-var connected_ids = allowed_ids.slice(0) // copies the content of allowed_ids
+var allowed_ids = []  //[8991608, 346751, 8991608, 111198, 112022, 115002] 
+var connected_ids = [] //allowed_ids.slice(0) // copies the content of allowed_ids
 set_shared_data('ALLOWED_IDS', allowed_ids)
 set_shared_data('CONNECTED_IDS', connected_ids)
 var t = get_shared_data('SQL_TABLES')
-var plugins = ['enocean_sensors/'] // Edit this array in order to load new plugins
+var plugins = ['enocean_sensors'] // Edit this array in order to load new plugins
 //******************************************************************
 
 var sensors_utils = require('./sensors')
@@ -61,11 +61,12 @@ function frame_to_android_notif (frame_data) {
 // @TODO : Put that logic somewhere else, like in the EventMonitor...
 function update_main_temperatures (frame_data) {
 	// Frame to be used as demo : A55A0B0700003608008933780084
-	if (frame_data.id == get_shared_data('OUT_TEMP_SENSOR_ID')) {
+	if (frame_data.id == get_shared_data('OUT_TEMP_SENSOR_HID')) {
 		console.log('The sensor id of the received frame is the one of the main OUTSIDE temperature sensor. Updating the server in-memory value.')
 		var temp = require('./sensors').decode_data_byte(1 /* temperature */, frame_data).toFixed(1)
 		set_shared_data('OUT_TEMP', temp)
-	} else if (frame_data.id == get_shared_data('IN_TEMP_SENSOR_ID')) {
+	} 
+	if (frame_data.id == get_shared_data('IN_TEMP_SENSOR_HID')) {
 		console.log('The sensor id of the received frame is the one of the main INSIDE temperature sensor. Updating the server in-memory value.')
 		var temp = require('./sensors').decode_data_byte(1 /* temperature */, frame_data).toFixed(1)
 		set_shared_data('IN_TEMP', temp)
@@ -96,13 +97,21 @@ function load_plugins () {
 var db = null
 var sensors_values = {}
 function GLOBAL_INIT () {
+	function cancel_startup (query) {
+		console.error("!! Error, could not load the former state of the sensors from the DB, aborting server startup. ¡¡")
+		console.error("The query that caused the error is " + query)
+		console.error("And the error is" + err)
+		process.exit()
+	}
 	console.log("Starting Initializing data...")
 	var ip = utils.getLocalPublicIpAddress(["eth0", "p2p1"])
 	set_shared_data('MAIN_SERVER_IP', ip)
 	set_shared_data('WEB_UI_HOME', 'http://' + ip + "/")
 	set_shared_data('MAIN_SERVER_PORT', 5000)
-	set_shared_data('IN_TEMP_SENSOR_ID', 8991608)
-	set_shared_data('OUT_TEMP_SENSOR_ID', 8991608)
+	set_shared_data('IN_TEMP_SENSOR_ID', 1)
+	set_shared_data('OUT_TEMP_SENSOR_ID', 1)
+	set_shared_data('IN_TEMP_SENSOR_HID', 8991608)
+	set_shared_data('OUT_TEMP_SENSOR_HID', 8991608)
 	db = new dbms.Database()
 	console.log("Connecting to db...")
 	db.connect('dat', function () {
@@ -110,22 +119,39 @@ function GLOBAL_INIT () {
 		console.log("DB connected.")
 		set_shared_data('IN_TEMP', 0) // @TODO : Get the value from the database instead !
 		set_shared_data('OUT_TEMP', -2) // @TODO : Get the value from the database instead !
-		var query = "SELECT sensor_id AS sid, MAX(time), value " +
-		"FROM `" + t['l'] + "` l " +
-		"GROUP BY sensor_id";//* /!\ According to StackOverflow, when using BTree as indexes (which is the case with sqlite), the maximum (key1, key2, key3) tuple will be the one returned by the GROUP BY and thus, for us, the last one in terms of time
-		db.select_query(query, null, function (err, rows) {
+		var q = "SELECT hardware_id FROM `" + t['s'] + "`"
+		db.select_query(q, null, function (err, rows) {
 			if (null != err) {
-				console.error("!! Error, could not load the former state of the sensors from the DB, aborting server startup. ¡¡")
-				console.error("The query that caused the error is " + query)
-				console.error("And the error is" + err)
-				process.exit()
-			};
-			for(var i in rows) {
-				sensors_values[rows[i].sid] = rows[i].value
+				cancel_startup(q)
 			}
-			console.log("Server startup states: " + JSON.stringify(sensors_values))
-			set_shared_data('SENSORS_VALUES', sensors_values) //* Will be updated by EventMonitor
-			start()
+			for(var i in rows) {
+				allowed_ids.push(rows[i].hardware_id)
+				connected_ids.push(rows[i].hardware_id)
+			}
+			
+			var query = 
+			" SELECT sensor_id AS sid, MAX(time), value " +
+			" FROM `" + t['l'] + "` l " +
+			" INNER JOIN `" + t['s'] + "` s ON (s.id = l.sensor_id)" +
+			" GROUP BY sensor_id";//* /!\ According to StackOverflow, when using BTree as indexes (which is the case with sqlite), the maximum (key1, key2, key3) tuple will be the one returned by the GROUP BY and thus, for us, the last one in terms of time
+			db.select_query(query, null, function (err, rows) {
+				if (null != err) {
+					cancel_startup(query)
+				}
+				for(var i in rows) {
+					if (rows[i].sid == get_shared_data('OUT_TEMP_SENSOR_ID')) {
+						set_shared_data('OUT_TEMP', parseFloat(rows[i].value).toFixed(1))
+					}
+					if (rows[i].sid == get_shared_data('IN_TEMP_SENSOR_ID')) {
+						set_shared_data('IN_TEMP', parseFloat(rows[i].value).toFixed(1))
+					}
+
+					sensors_values[rows[i].sid] = rows[i].value
+				}
+				console.log("Server startup states: " + JSON.stringify(sensors_values) + JSON.stringify(allowed_ids) + JSON.stringify(connected_ids))
+				set_shared_data('SENSORS_VALUES', sensors_values) //* Will be updated by EventMonitor
+				start()
+			})
 		})
 	})
 }
