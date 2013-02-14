8c7d065c6e7c01d6aafb4ecbd50676f6b634ad7b
tdubourg <plop@plop.fake>
2013-02-14 15:55:15 +0100

    var name error + missing export

--- js/server/plugins/enocean_sensors/events_handlers.js
+++ js/server/plugins/enocean_sensors/events_handlers.js
@@ -1,5 +1,6 @@
 "use strict"
 var eventsMonitor = require('../../events_monitor')
+var checkThresholds = eventsMonitor.checkThresholds
 var shared_data = require('../../shared_data')
 
 function tempEvent(idSensor, sensor_type_id, value) {
