c8b4a745354ce475a0564c512a5690257e9ec6fa
selimabisaber <selimabisaber@gmail.com>
2013-02-14 15:49:57 +0100

    ...

--- js/server/mainserver_simulator.js
+++ js/server/mainserver_simulator.js
@@ -11,7 +11,7 @@ function generateSimulations(){
 	//var simulation_type = '';
 	var frames = {}
 	var quantum = 3000
-	 //lumino
+
 	frames[1] = { "data" : "A55A0B07A623000D00054A7F00B6" , "type" : "luminosity" } 
 	frames[2] = { "data" : "A55A0B07A82A000D00054A7F00BF" , "type" : "luminosity" } 
 	frames[3] = { "data" : "A55A0B07AA47000D00054A7F00DE" , "type" : "luminosity" } 
@@ -19,15 +19,23 @@ function generateSimulations(){
 	frames[5] = { "data" : "A55A0B0700003608008933780084" , "type" : "temperature" } 
 	frames[6] = { "data" : "A55A0B06000000090001B5960066" , "type" : "capteur contact 1" }
 	frames[7] = { "data" : "A55A0B06000000080001B25E002A" , "type" : "capteur contact 2" }
+		
 	frames[8] = { "data" : "A55A0B07052200480019337800A0" , "type" : "electricity 1" }
-	var firstvalue = 1
-	var lastvalue  = 1
+	
 
-	setInterval(function(){ 
-		var temp = Math.floor((Math.random()*lastvalue)+firstvalue);
-		sendData(frames[temp].data) // send simulation frame to sensor_server
-		console.log('===> [SIMULATION_FRAME_SENT] : Type '+frames[temp].type)
-	}, quantum);
+	frames[11] = {"data" : "A55A0B07000052080089337800A0"}
+
+	var firstvalue = 8
+	var lastvalue  = 8
+
+
+	sendData(frames[11].data)
+
+	// setInterval(function(){ 
+	// 	var temp = Math.floor((Math.random()*lastvalue)+firstvalue);
+	// 	sendData(frames[temp].data) // send simulation frame to sensor_server
+	// 	console.log('===> [SIMULATION_FRAME_SENT] : Type '+frames[temp].type)
+	// }, quantum);
 }
 
 
