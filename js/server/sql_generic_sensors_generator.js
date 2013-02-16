6f7792114ffd94a349050c35c57a89c9e9d268ff
selimabisaber <selimabisaber@gmail.com>
2013-02-16 14:15:21 +0100

    Simulations done

--- js/server/sql_generic_sensors_generator.js
+++ js/server/sql_generic_sensors_generator.js
@@ -1,13 +1,15 @@
-console.log('delete from sensors')
+console.log('delete from sensors;')
 
 var compteur = 1
 var dec_start =  8991609  // <=>  // var hex_start = 00893379
 
 for (i  = 1 ; i <= 4000 ; i++) {
 	formated_hex = ("0000000000000" + dec_start.toString(16)).substr(-8);
-	sql = "insert into sensors values (null, "+dec_start+", 'Generic_Sensor_"+(compteur++)+"', 5)"
+	//sql = "insert into sensors values (null, "+dec_start+", 'Generic_Sensor_"+(compteur++)+"', 5);"
+	frame = 'frames['+(compteur++)+'] = { "data" : "A55A0B0700005208'+formated_hex.toUpperCase()+'00A0" , "type" : "Generic_frame_'+(compteur-1)+'" }' 
 	dec_start++
-	console.log("SQL: "+sql)
+	//console.log("SQL: "+sql)
+	console.log(frame)
 	//console.log("Frame: A55A0B0700005208"+formated_hex+"00A0")
 }
 
