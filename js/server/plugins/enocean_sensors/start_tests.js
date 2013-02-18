966c95a8ea116a74b41ab59f27102ba759ccc604
tdubourg <plop@plop.fake>
2013-02-18 22:58:39 +0100

    Giving the user a bit more time to read messages

--- js/server/plugins/enocean_sensors/start_tests.js
+++ js/server/plugins/enocean_sensors/start_tests.js
@@ -12,32 +12,32 @@ var sutils = require('../../sensors')
 //* Action for device of type 2 
 start_tests[1] = function (req, res, params, testid) {
 	console.log("STARTT: Starting testid=", testid, "satr =", JSON.stringify(satr[testid]))
-	res.end(JSON.stringify({'testid': testid, hideafter: 1000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
+	res.end(JSON.stringify({'testid': testid, hideafter: 3000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
 }
 //* Action for device of type 2 
 start_tests[2] = function (req, res, params, testid) {
 	console.log("STARTT: Starting testid=", testid)
-	res.end(JSON.stringify({'testid': testid, hideafter: 1000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
+	res.end(JSON.stringify({'testid': testid, hideafter: 3000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
 }
 //* Action for device of type 2 
 start_tests[3] = function (req, res, params, testid) {
 	console.log("STARTT: Starting testid=", testid)
-	res.end(JSON.stringify({'testid': testid, hideafter: 1000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
+	res.end(JSON.stringify({'testid': testid, hideafter: 3000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
 }
 //* Action for device of type 2 
 start_tests[4] = function (req, res, params, testid) {
 	console.log("STARTT: Starting testid=", testid)
-	res.end(JSON.stringify({'testid': testid, hideafter: 1000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
+	res.end(JSON.stringify({'testid': testid, hideafter: 3000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
 }
 //* Action for device of type 5 : Power switch plug
 start_tests[5] = function (req, res, params, testid) {
 	console.log("STARTT: Starting testid=", testid)
 	satr[testid]['start_time'] = new Date()
 	deviceCommunicator.sendToSensorWithDevType(parseInt(params.query.deviceId), 5, "ON")
-	res.end(JSON.stringify({'testid': testid, hideafter: 1500, msg: "Votre prise va être mise en état ON pendant 7 secondes puis éteinte." }))
+	res.end(JSON.stringify({'testid': testid, hideafter: 3000, msg: "Votre prise va être mise en état ON pendant 7 secondes puis éteinte." }))
 }
 //* Action for device of type 2 
 start_tests[6] = function (req, res, params, testid) {
-	res.end(JSON.stringify({'testid': testid, hideafter: 1000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
+	res.end(JSON.stringify({'testid': testid, hideafter: 3000, msg: "Veuillez maintenant appuyer sur le bouton LRN de votre capteur." }))
 }
 
